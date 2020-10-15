b643230ce4879809f205cadd136cae85bbf77787
PhantomOz <faniogor@gmail.com>
2020-10-15 12:38:09 +0100

    first major task

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    //========== First Major Task ============
+
+    /* 
+        Occupation: Student
+        Gender: Male
+        Side Hustle Internship Username: PhantomOz
+
+    */
+
+    //About me Title
+    echo "<h1>About Me</h1> <hr/>";
+    //Side Hustle Internship Username
+    echo "<h3>PhantomOz</h3>";
+    //about myself
+    echo "<p>Hello, I am PhantomOz a student of uniben. I love Coding and playing fifa</p>";
+
+
+?>
